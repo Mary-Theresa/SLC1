1fe717b2cc291b822c006a08be7c7e3dfd032451
Mary-Theresa <mtnwankwo@gmail.com>
2017-03-11 10:13:23 +0100

    Arithmetic case solved

---
+++ Apps/aritGeo.js
@@ -0,0 +1,38 @@
+module.exports={
+
+aritGeo: function(arr)
+{
+	
+	var diff = (arr[1] - arr[0])
+	var ratio = (arr[1] / arr[0]);
+	var ap = 1
+	var gp = 1
+
+	if (arr.length === 0)
+	{
+		return 0;
+	}
+	for (i=1; i<=arr.length-1; i++)
+	  {
+	    if (diff === (arr[i+1] - arr[i]))
+	    {
+	      ap++
+	    }
+	  } 
+	
+
+if (ap == arr.length-1)
+{
+  return 'Arithmetic'
+}
+else if (gp == arr.length-1)
+{
+  return 'Geometric'
+}
+else 
+{
+  return -1
+}
+}
+}
+
