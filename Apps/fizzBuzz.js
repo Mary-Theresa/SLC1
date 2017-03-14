8e842e37c8cb4d993cb2bfe5cf16f6d4cf4c0296
Mary-Theresa <mtnwankwo@gmail.com>
2017-03-14 12:48:58 +0100

    Unnecessary files removed

--- Apps/fizzBuzz.js
+++ Apps/fizzBuzz.js
@@ -2,10 +2,24 @@ module.exports={
 
 fizzBuzz: function(number)
 {
-	if (number % 3 === 0)
+	if ((number % 3 === 0) && (number % 5 === 0))
+	{
+		return 'FizzBuzz';
+	}
+	else if (number % 3 === 0)
 	{
 		return 'Fizz';
 	}
+	else if (number % 5 === 0)
+	{
+		return 'Buzz';
+	}
+	
+	else
+	{
+		return number;
+	}
 }
+
 };
 	
